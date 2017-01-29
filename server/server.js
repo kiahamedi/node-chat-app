c742c634fd7300b60a84efc80182757fa1adaab6
mayankamencherla <mayank.amencherla@razorpay.com>
2017-01-29 18:14:36 +0530

    Added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 // Setting up modules
 const path       = require('path');
+const http       = require('http');
 const express    = require('express');
-
+const socketIO   = require('socket.io');
 // Setting up local variables
 const publicPath = path.join(__dirname, '..', 'public');
 const PORT = process.env.PORT || 3000;
 
 // Setting up express 
-var app = express();
+var app = express();                        // uses http server
+var server = http.createServer(app); 
+var io = socketIO(server);                  // communicate between server and client
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+// Listen to a new connection from client
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`App succesfully started on ${PORT}`);
 });
