b937310784a18951835af935c6e0100a43425273
mayankamencherla <mayank.amencherla@razorpay.com>
2017-01-29 15:21:41 +0530

    Modified package.json

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const express    = require('express');
 
 // Setting up local variables
 const publicPath = path.join(__dirname, '..', 'public');
+const PORT = process.env.PORT || 3000;
 
 // Setting up express 
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('App succesfully started');
+app.listen(PORT, () => {
+    console.log(`App succesfully started on ${PORT}`);
 });
